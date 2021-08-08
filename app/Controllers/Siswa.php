d987765c43f31c3abb025b311b352b416957c979
Widyantoalif <alifwidiyanto46@gmail.com>
2021-08-08 22:02:48 +0700

    update data

--- app/Controllers/Siswa.php
+++ app/Controllers/Siswa.php
@@ -80,4 +80,66 @@ class Siswa extends Controller
             return redirect()->to(base_url('/siswa'));
         }
     }
+    public function ubah()
+    {
+        if (isset($_POST['ubah'])) {
+            $id = $this->request->getPost('id');
+            $nisn = $this->request->getpost('nisn');
+            $db_nisn = $this->model->getDataById($id)['nisn'];
+
+            if ($nisn === $db_nisn) {
+                $val = $this->validate([
+                    'nisn' => [
+                        'label' => 'Nomor Induk Siswa Nasional',
+                        'rules' => 'required|numeric|max_length[12]'
+                    ],
+                    'nama' => [
+                        'label' => 'Nama Siswa',
+                        'rules' => 'required'
+                    ]
+                ]);
+            } else {
+                $val = $this->validate([
+                    'nisn' => [
+                        'label' => 'Nomor Induk Siswa Nasional',
+                        'rules' => 'required|numeric|max_length[12]|is_unique[siswa.nisn]'
+                    ],
+                    'nama' => [
+                        'label' => 'Nama Siswa',
+                        'rules' => 'required'
+                    ]
+                ]);
+            }
+
+
+            if (!$val) {
+                session()->setflashdata('err', \Config\Services::validation()->listErrors());
+                $data = [
+                    'judul' => 'Data Siswa',
+                    'siswa' => $this->model->getAllData()
+                ];
+                echo view('Templates/v_header', $data);
+                echo view('Templates/v_sidebar');
+                echo view('Templates/v_topbar');
+                echo view('Siswa/index', $data);
+                echo view('Templates/v_footer');
+            } else {
+                $id = $this->request->getPost('id');
+                $data = [
+                    'nisn' => $this->request->getPost('nisn'),
+                    'nama' => $this->request->getPost('nama')
+
+                ];
+
+                //update data
+                $success = $this->model->ubah($data, $id);
+                if ($success) {
+                    session()->setFlashdata('message', 'diubah');
+                    return redirect()->to(base_url('/siswa'));
+                }
+            }
+        } else {
+            return redirect()->to(base_url('/siswa'));
+        }
+    }
 }
--- app/Models/M_Siswa.php
+++ app/Models/M_Siswa.php
@@ -6,23 +6,39 @@ use CodeIgniter\Model;
 
 class M_Siswa extends Model
 {
+    protected $table = 'siswa';
     public function __construct()
     {
         $this->db = db_connect();
+        $this->builder = $this->db->table($this->table);
     }
 
     public function getAllData()
     {
+        //return $this->builder->get();
         return $this->db->table('siswa')->get()->getResultArray();
+    }
+
+    public function getDataById($id)
+    {
+        $this->builder->where('id', $id);
+        return $this->builder->get()->getRowArray();
     }
 
     public function tambah($data)
     {
-        return $this->db->table('siswa')->insert($data);
+        return $this->builder->insert($data);
+        //return $this->db->table('siswa')->insert($data);
     }
 
     public function hapus($id)
     {
-        return $this->db->table('siswa')->delete(['id' => $id]);
+        return $this->builder->delete(['id' => $id]);
+        //return $this->db->table('siswa')->delete(['id' => $id]);
+    }
+
+    public function ubah($data, $id)
+    {
+        return $this->builder->update($data, ['id' => $id]);
     }
 }
